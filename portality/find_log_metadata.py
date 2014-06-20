b80a6b59266e5c03c1b0a42a4e57b7188be7376b
Steven Eardley <me@steveneardley.co.uk>
2014-06-20 16:01:29 +0100

    Parses whois just fine, thank you very much.

--- portality/find_log_metadata.py
+++ portality/find_log_metadata.py
@@ -1,7 +1,16 @@
 __author__ = 'steve'
 """
 Find some other interesting information not written to the logs, and add these to the entry.
+Copyright information can be found here: http://www.apnic.net/db/dbcopyright.html, and states:
+
+ " Users will not be able to download the full contents of the database unless the intended use
+   is for "Internet operational issues". These words are tightly defined and would include network
+   trouble-shooting, abuse reporting, and Internet research and analysis. "
+
+This use falls under 'abuse reporting'.
 """
+
+import re
 from portality.core import app
 from pygeoip import GeoIP
 from random import randint
@@ -10,16 +19,27 @@ from ipwhois import IPWhois
 geoip = GeoIP(app.config['GEOIP_PATH'])
 
 def lookup_location(attack_model):
+    """
+    The GeoIP database configured in app.config allows lookup of attacker's location.
+    :param attack_model: an SshEntry to add the location to.
+    :return: a dict of location information.
+    """
     ip = attack_model.data['attack_ip']
     return geoip.record_by_addr(ip)
 
 def lookup_whois(attack_model):
+    """
+    Find and parse the whois information for an IP address, and store it in an index.
+    Each whois takes some time; the index considerably improves speed for repeated attacks
+    (plus prevents spamming).
+    :param attack_model: an SshEntry to add whois information to
+    :return: the name of the attacker
+    """
     from portality.models import NameAndShameEntry
     ip = attack_model.data['attack_ip']
 
     # Attempt to find a stored name
     name_query = NameAndShameEntry.query(q={"query":{"match":{'id':ip}}})
-    print(ip)
 
     # If we don't find the IP in our index, run whois and create it
     n_matches = name_query['hits']['total']
@@ -37,11 +57,31 @@ def lookup_whois(attack_model):
         exit(1)
 
 def run_whois(ip):
+    """
+    Run the whois, given an IP
+    :param ip: string encoded IP address
+    :return: the name of the attacker
+    """
     who = IPWhois(ip).lookup(inc_raw=True)
 
     # The IPWhois package doesn't give us the name information, so we parse the raw information.
     raw_whois = who['raw']
 
-    return randint(0, 10000)
+    whois_lines = raw_whois.splitlines()
+
+    # for now, just naively grab lines that might relate to people. Works well for APNIC whois lookups.
+    name_lines = list(filter(lambda l: l.startswith("person:"), whois_lines))
+
+    # Alternatively, use the method below to try to catch more
+    # match_indicators = re.compile('|'.join(["person", "name"]), re.IGNORECASE)
+    # name_lines = list(filter(lambda l: match_indicators.search(l), whois_lines))
+    print(ip)
+    print(name_lines)
+    name = "unknown"
+    # Lines are 'label: info', so discard label and strip whitespace.
+    if len(name_lines) > 0:
+        name = name_lines[0].rpartition(':')[2].lstrip()
+    print(name)
+    return name
 
 
