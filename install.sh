1a8c3eba5ac24dec28ce8ce1423e1dd5066efbc1
Steven Eardley <me@steveneardley.co.uk>
2014-08-11 15:45:18 +0100

    Improved the install script some more.

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #!/bin/sh
 
 # Activate the virtualenv
-source ../bin/activate
 
-# Install python dependencies
+env = 0
+echo -n "Install script for SSH Attacks.\n
+Has the virtualenv been activated? (Y/n): "
+
+read response
+
+if ($response == "n") || ($response == "N")
+then
+    echo "Activating virtualenv now"
+    source ../bin/activate
+    $env =1
+else
+    echo "OK, I'll trust you. continuing the installation"
+fi
+
+echo "Installing Python dependencies"
 pip install -e .
 
-# Install git submodules
+echo "Installing git submodules"
 git submodule init && git submodule update
 
-# Make a copy of the settings file
+echo "Making a copy of the settings file"
 cp portality/settings.py ./app.cfg
 
+echo "Getting GeoLite data"
 # Create a directory for the data
 mkdir data && cd data
 
@@ -19,7 +34,9 @@ mkdir data && cd data
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 gunzip GeoLiteCity.dat.gz
 
-deactivate
+if ($env == 1)
+    deactivate
+fi
 
 echo "Installation complate. Check and customise the settings in app.cfg
 before running SSH Attacks."
