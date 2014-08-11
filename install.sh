04d72775def967b2c576c9bc59d24363f073a67b
Steven Eardley <me@steveneardley.co.uk>
2014-08-11 15:31:03 +0100

    added virtualenv thing to install script. Might be needed....

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Activate the virtualenv
+source ../bin/activate
+
 # Install python dependencies
 pip install -e .
 
@@ -15,6 +18,8 @@ mkdir data && cd data
 # Download and extract the Geolite data file
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 gunzip GeoLiteCity.dat.gz
+
+deactivate
 
 echo "Installation complate. Check and customise the settings in app.cfg
 before running SSH Attacks."
