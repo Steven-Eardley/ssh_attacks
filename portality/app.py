c5e705d2a46d9476fdfdef285c4d90c73c085ce5
Steven Eardley <me@steveneardley.co.uk>
2014-08-11 11:47:19 +0100

    Removed lots of unused portality functions from ssh_attacks.

--- portality/app.py
+++ portality/app.py
@@ -6,32 +6,21 @@ also be backed up by models, so have a look at the example models and use them /
 new ones as required too.
 '''
 
-from flask import Flask, request, abort, render_template
-from flask.views import View
+from flask import request, render_template
 from flask_login import login_user, current_user
 
 import portality.models as models
 from portality.core import app, login_manager
 
-from portality.view.account import blueprint as account
-from portality.view.nav import blueprint as nav
-from portality.view.admin import blueprint as admin
-from portality.view.graph import blueprint as graph
 from portality.view.contact import blueprint as contact
 from portality.view.query import blueprint as query
-from portality.view.package import blueprint as package
-from portality.view.forms import blueprint as forms
 from portality.view.data import blueprint as data
 from portality.view.explorer import blueprint as explorer
 from portality.view.map import blueprint as ip_map
-from portality.view.pagemanager import blueprint as pagemanager
 
-app.register_blueprint(nav, url_prefix='/nav')
-app.register_blueprint(graph, url_prefix='/graph')
+
 app.register_blueprint(contact, url_prefix='/contact')
 app.register_blueprint(query, url_prefix='/query')
-app.register_blueprint(package, url_prefix='/package')
-app.register_blueprint(forms, url_prefix='/forms')
 app.register_blueprint(data, url_prefix='/data')
 app.register_blueprint(explorer, url_prefix='/explorer')
 app.register_blueprint(ip_map, url_prefix='/map')
@@ -66,7 +55,6 @@ def standard_authentication():
             user = models.Account.pull(res[0]['_source']['id'])
             if user:
                 login_user(user, remember=False)
-
 
 @app.errorhandler(404)
 def page_not_found(e):
